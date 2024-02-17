6cd13d8b213d2f87677974f751f47a23a7db3909
Sunderinq <tegnoor.gill@hotmail.com>
2024-02-16 18:31:43 -0800

    p1 complete

--- p1/P1_TEGNOOR_GILL.hs
+++ p1/P1_TEGNOOR_GILL.hs
@@ -1,4 +1,6 @@
 import qualified Data.Map.Strict as Map
+import System.Environment (getArgs)
+
 
 type VarAsgn = Map.Map VarId Bool
 type VarId = String
@@ -14,42 +16,42 @@ data Prop = Const Bool
 
 findVarIds :: Prop -> [VarId]
 findVarIds (Const _) = []
-findVarIds (Var v) = [v]
+findVarIds (Var p) = [p]
 findVarIds (Not p) = findVarIds p
-findVarIds (And p p1) = findVarIds p ++ findVarIds p1
-findVarIds (Or p p1) = findVarIds p ++ findVarIds p1
-findVarIds (Imply p p1) = findVarIds p ++ findVarIds p1
-findVarIds (Iff p p1) = findVarIds p ++ findVarIds p1
+findVarIds (And p1 p2) = findVarIds p1 ++ findVarIds p2
+findVarIds (Or p1 p2) = findVarIds p1 ++ findVarIds p2
+findVarIds (Imply p1 p2) = findVarIds p1 ++ findVarIds p2
+findVarIds (Iff p1 p2) = findVarIds p1 ++ findVarIds p2
 
-genVarAsgns :: [VarId] -> [VarAsgn] 
-genVarAsgns vars = map (Map.fromList . zip vars) (sequence (replicate (length vars) [True, False]))
+genVarAsgns :: [VarId] -> [VarAsgn]
+genVarAsgns [] = [Map.empty]
+genVarAsgns (x:xs) = [Map.insert x b a | b <- [True, False], a <- genVarAsgns xs]
 
 eval :: Prop -> VarAsgn -> Bool
-eval (Const p) _ = p
-eval (Var p) a = case Map.lookup p a of
-    Just p -> p
-    Nothing -> error "error"
+eval (Const b) a = b
+eval (Var p) a = Map.findWithDefault False p a
 eval (Not p) a = not (eval p a)
 eval (And p1 p2) a = eval p1 a && eval p2 a
-eval (Or p1 p2) a = eval p1 a || eval p2 a
-eval (Imply p1 p2) a = not (eval p1 a) || eval p2 a
-eval (Iff p1 p2) a = eval p1 a == eval p2 a
+eval (Or p1 p2) a = eval p1 a || eval p2 a 
+eval (Imply p1 p2) a = not (eval p1 a) || eval p2 a 
+eval (Iff p1 p2) a = eval p1 a == eval p2 a 
 
-sat :: Prop -> Bool 
-sat p = any (eval p) (genVarAsgns (findVarIds p))
+sat :: Prop -> Bool
+sat p = any (eval p) (genVarAsgns (findVarIds  p))
 
 readFormula :: String -> Prop
-readFormula  = read
+readFormula = read
 
 checkFormula :: String -> String
-checkFormula formulaStr =
-    if sat (readFormula formulaStr)
-        then "SAT"
-        else "UNSAT"
+checkFormula s 
+    | sat(readFormula s) = "SAT"
+    | otherwise = "UNSAT"
 
 main :: IO ()
 main = do
-    contents <- readFile "formulas.txt"
-    mapM_ (putStrLn . checkFormula) (lines contents) 
-
-
+    args <- getArgs
+    case args of
+        [fileName] -> do
+            contents <- readFile fileName
+            let formulaStrings = lines contents
+            mapM_ (putStrLn . checkFormula) formulaStrings
